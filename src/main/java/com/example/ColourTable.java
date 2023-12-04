d585e5c9118bbac64c5a4fd0993e9f12364d34b1
yachitrasivakumar <yaya.s@hotmail.co.uk>
2023-12-04 18:39:16 +0000

    Created test to handle duplicate colours and 24-bit colour validation, wrote code to allow these tests to pass

--- src/main/java/com/example/ColourTable.java
+++ src/main/java/com/example/ColourTable.java
@@ -1,10 +1,12 @@
 package com.example;
-
 import java.util.ArrayList;
 
 public class ColourTable {
     int max;
     ArrayList colours;
+    public static void main(String args[]) {
+        System.out.println("hi");
+    }
     public ColourTable() {
         throw new IllegalArgumentException("Must specify size of Colour Table");
     }
@@ -15,11 +17,19 @@ public class ColourTable {
         this.colours = new ArrayList<>(size);
         this.max = size;
     }
-
     public void add(int rgb) {
         if (colours.size() >= max) {
             throw new IllegalArgumentException("Exceeded the capacity of the ColourTable.");
         }
-        colours.add(rgb);
+        if ((rgb & 0xFF000000) != 0) {
+            throw new IllegalArgumentException("Invalid 24-bit RGB value.");
+        }
+        if (!colours.contains(rgb)) {
+            // only adds unique colours to palette
+            colours.add(rgb);
+        }
+    }
+    public int length() {
+        return colours.size();
     }
 }
--- src/test/java/ColourTableTest.java
+++ src/test/java/ColourTableTest.java
@@ -6,7 +6,6 @@ public class ColourTableTest {
     void testValidConstructor() {
         assertDoesNotThrow(() -> new ColourTable(8));
     }
-
     @Test
     void testConstructorInvalidSize() {
         assertThrows(IllegalArgumentException.class, () -> new ColourTable(3));
@@ -19,13 +18,11 @@ public class ColourTableTest {
         assertDoesNotThrow(() -> new ColourTable(4));
         assertDoesNotThrow(() -> new ColourTable(16));
     }
-
     @Test
     void testAddValidColor() {
         ColourTable table = new ColourTable(4);
         assertDoesNotThrow(() -> table.add(0xFFFFFF)); // White color in RGB
     }
-
     @Test
     void testExceedingCapacityThrowsException() {
         ColourTable table = new ColourTable(2);
@@ -33,5 +30,18 @@ public class ColourTableTest {
         table.add(0x000000);
         assertThrows(IllegalArgumentException.class, () -> table.add(0xFF0000)); // Third addition will cause exception
     }
-
+    @Test
+    void testDuplicateColourAddition() {
+        ColourTable table = new ColourTable(2);
+        // add same colour twice
+        table.add(0xFFFFFF);
+        table.add(0xFFFFFF);
+        assertEquals(1, table.length());
+    }
+    @Test
+    void testValid24BitRGBValue() {
+        ColourTable table = new ColourTable(2);
+        // add something that isn't a 24-bit RGB value
+        assertThrows(IllegalArgumentException.class, () -> table.add(0xFFFFFFF));
+    }
 }
